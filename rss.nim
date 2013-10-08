b17e746b3058d3a2f257ecf3626201bc8365eac0
achesak <achesak@yahoo.com>
2013-10-08 18:10:29 -0500

    First commit of main file.

---
+++ rss.nim
@@ -0,0 +1,11 @@
+# Nimrod RSS (Really Simple Syndication) module
+
+# Written by Adam Chesak.
+# Code released under the MIT open source license.
+
+# Import modules.
+import httpclient
+import strutils
+import xmlparser
+import xmltree
+import streams
