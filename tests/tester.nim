53296c2a604610da5e2cb69a9ba94b00767ff357
Shigeaki Matsumura <matsu911@gmail.com>
2018-09-03 10:29:49 +0900

    Fix parsing RDF items and refacotring

--- tests/tester.nim
+++ tests/tester.nim
@@ -23,6 +23,23 @@ test "parse rss file":
   check(feed.skipHours == nil)
   check(feed.skipDays == nil)
   check(feed.items.len == 9)
+  let item = feed.items[0]
+  check(item.title == "RSS Solutions for Restaurants")
+  check(item.link == "http://www.feedforall.com/restaurant.htm")
+  check(item.description == """<b>FeedForAll </b>helps Restaurant's communicate with customers. Let your customers know the latest specials or events.<br>
+<br>
+RSS feed uses include:<br>
+<i><font color="#FF0000">Daily Specials <br>
+Entertainment <br>
+Calendar of Events </i></font>""")
+  check(item.author == nil)
+  check(item.category == @["Computers/Software/Internet/Site Management/Content Management"])
+  check(item.comments == "http://www.feedforall.com/forum")
+  check(item.enclosure == RSSEnclosure())
+  check(item.guid == nil)
+  check(item.pubDate == "Tue, 19 Oct 2004 11:09:11 -0400")
+  check(item.sourceUrl == nil)
+  check(item.sourceText == nil)
 
 test "parse rdf file":
   let feed = loadRSS("./tests/test.rdf")
@@ -46,3 +63,32 @@ test "parse rdf file":
   check(feed.skipHours == nil)
   check(feed.skipDays == nil)
   check(feed.items.len == 185)
+  let item = feed.items[0]
+  check(item.title == "QuasarNET: Human-level spectral classification and redshifting with Deep Neural Networks. (arXiv:1808.09955v1 [astro-ph.IM])")
+  check(item.link == "http://arxiv.org/abs/1808.09955")
+  check(item.description == """<p>We introduce QuasarNET, a deep convolutional neural network that performs
+    classification and redshift estimation of astrophysical spectra with
+    human-expert accuracy. We pose these two tasks as a \emph{feature detection}
+    problem: presence or absence of spectral features determines the class, and
+    their wavelength determines the redshift, very much like human-experts proceed.
+    When ran on BOSS data to identify quasars through their emission lines,
+    QuasarNET defines a sample $99.51\pm0.03$\% pure and $99.52\pm0.03$\% complete,
+    well above the requirements of many analyses using these data. QuasarNET
+    significantly reduces the problem of line-confusion that induces catastrophic
+    redshift failures to below 0.2\%. We also extend QuasarNET to classify spectra
+    with broad absorption line (BAL) features, achieving an accuracy of
+    $98.0\pm0.4$\% for recognizing BAL and $97.0\pm0.2$\% for rejecting non-BAL
+    quasars. QuasarNET is trained on data of low signal-to-noise and medium
+    resolution, typical of current and future astrophysical surveys, and could be
+    easily applied to classify spectra from current and upcoming surveys such as
+    eBOSS, DESI and 4MOST.
+    </p>
+    """)
+  check(item.author == """<a href="http://arxiv.org/find/astro-ph/1/au:+Busca_N/0/1/0/all/0/1">Nicolas Busca</a>, <a href="http://arxiv.org/find/astro-ph/1/au:+Balland_C/0/1/0/all/0/1">Christophe Balland</a>""")
+  check(item.category == nil)
+  check(item.comments == nil)
+  check(item.enclosure == RSSEnclosure())
+  check(item.guid == nil)
+  check(item.pubDate == nil)
+  check(item.sourceUrl == nil)
+  check(item.sourceText == nil)
